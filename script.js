3ea5c0b878d50b8e6c0d800e0e2112c33ea17682
Maryan Smerchinkiy <110513102+Marsmers@users.noreply.github.com>
2023-07-30 22:20:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// let age = prompt("Скільки тобі років?")
+// alert(`Вам ${age} років`)
+
+
+
+
+// let celsius = prompt("Вкажіть температуру в градусах цельсія")
+// let fahrenheit = (celsius*9/5+32)
+// alert(`Температура в фаренгейтах становить ${fahrenheit}`) 
+
+
+
+// let value = prompt("Вкажіть число")
+// let result = Math.floor(value/2)
+// alert (result)
+
+
+// const rate = 36.69
+// let uah = prompt("Вкажіть кількість гривень")
+// let cash = uah/rate
+// alert (cash.toFixed(2))
+
+
+// let red = prompt("Вкажіть значення від 0 до 255")
+// let green = prompt("Вкажіть значення від 0 до 255")
+// let blue = prompt("Вкажіть значення від 0 до 255")
+// let hexRed = parseInt(red).toString(16);
+// let hexGreen = parseInt(green).toString(16);
+// let hexBlue = parseInt(blue).toString(16);
+// let color = "#" + hexRed + hexGreen + hexBlue;
+// alert("Колір: " + color);
+
+
+
+// let numFloors = prompt("Введіть кількість поверхів у будинку:");
+// let numFlatsPerFloor = prompt("Введіть кількість квартир на поверсі:");
+// let flatNumber = prompt("Введіть номер квартири:");
+// let entranceNumber = Math.ceil(flatNumber / (numFloors * numFlatsPerFloor));
+// let floorNumber = Math.ceil((flatNumber - (entranceNumber - 1) * numFloors * numFlatsPerFloor) / numFlatsPerFloor);
+// alert("Квартира з номером " + flatNumber + " знаходиться в " + floorNumber + " під'їзді та " + entranceNumber + " поверсі.");
