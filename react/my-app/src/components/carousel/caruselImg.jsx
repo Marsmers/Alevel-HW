be1922463cc6085f8296cc3668efbed637c9670c
Maryan Smerchinkiy <maryan.smerchinskiy@gmail.com>
2023-10-08 12:40:42 +0300

    commit

---
+++ react/my-app/src/components/carousel/caruselImg.jsx
@@ -0,0 +1,10 @@
+
+ const imageArr = [
+    'https://images.wallpaperscraft.ru/image/single/pejzazh_holmy_derevia_1018523_3840x2160.jpg',
+    'https://images.wallpaperscraft.ru/image/single/gory_les_tuman_941650_3840x2160.jpg',
+    'https://images.wallpaperscraft.ru/image/single/derevia_svet_luchi_1024517_3840x2160.jpg',
+    'https://images.wallpaperscraft.ru/image/single/holmy_sneg_reka_924765_3840x2160.jpg',
+    'https://images.wallpaperscraft.ru/image/single/les_derevia_tuman_1053541_3840x2160.jpg',
+    'https://images.wallpaperscraft.ru/image/single/derevia_les_sneg_912918_3840x2160.jpg'];
+  
+export default imageArr
